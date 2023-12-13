64c4cd20c3c53b80a12adc03e9782200860ee49d
Rayssa <rayssatsouza10@gmail.com>
2023-12-13 16:30:31 -0300

    Correção da pagina Home (imagens corretas)

--- src/components/HomeBanner/ImagemFundo.jsx
+++ src/components/HomeBanner/ImagemFundo.jsx
@@ -1,11 +1,40 @@
+import { useEffect, useState } from 'react';
 import {getUser} from './../../services/storage'
+import { get } from '../../services/http';
 
 export default function ImagemFundo(){
     const usuario = getUser();
+    const [atleta, setAtleta] = useState({})
+    const [load, setLoad] = useState(false);
+
+    console.log("Usuario", usuario)
+    console.log("Atleta", atleta)
+
+    const getAtleta = () => {
+        setLoad(true)
+        get(`api/atleta/${usuario.atleta_id}`).then((response) => {
+            setAtleta(response.data)
+            console.log(response.data)
+            setLoad(false)
+        }).catch((erro) => {
+            setSnackOptions(prev => ({
+                mensage: erro?.response?.data?.message ? erro.response.data.message : erro?.message ? erro.message : 'Unespected error appears',
+                type: "error",
+                open: true
+            }));
+        });
+    }
+
+    useEffect(() => {
+        if (usuario?.perfil_id == 5){
+            getAtleta();
+        }
+    }, []);
+
     
     return(
-        usuario.perfil_id == 1 ? 
-        <img className="opacity-50" style={{width:'1190px', height: '480px'}} src="equipe.jpeg" />
-        : <img className="opacity-50" style={{width:'1190px', height: '480px'}} src="SUB-11.jpeg" />
+        usuario.perfil_id == 5 ? (
+            <img className="opacity-50" style={{width:'1190px', height: '480px'}} src={atleta.categoria?.caminhoImagem} />
+        ): <img className="opacity-50" style={{width:'1190px', height: '480px'}} src="equipe.jpeg" />
     )
 }
--- src/components/HomeBanner/ImagemPrincipal.jsx
+++ src/components/HomeBanner/ImagemPrincipal.jsx
@@ -1,9 +1,43 @@
+import { useEffect, useState } from 'react';
+import {getUser} from './../../services/storage'
+import { get } from '../../services/http';
+
 export default function ImagemPrincipal(){
+    const usuario = getUser();
+    const [atleta, setAtleta] = useState({})
+    const [load, setLoad] = useState(false);
+
+    console.log("Usuario", usuario)
+    console.log("Atleta", atleta)
+
+    const getAtleta = () => {
+        setLoad(true)
+        get(`api/atleta/${usuario.atleta_id}`).then((response) => {
+            setAtleta(response.data)
+            console.log(response.data)
+            setLoad(false)
+        }).catch((erro) => {
+            setSnackOptions(prev => ({
+                mensage: erro?.response?.data?.message ? erro.response.data.message : erro?.message ? erro.message : 'Unespected error appears',
+                type: "error",
+                open: true
+            }));
+        });
+    }
+
+    useEffect(() => {
+        if (usuario?.perfil_id == 5){
+            getAtleta();     
+        }
+    }, []);
+
     return(
+        usuario.perfil_id == 5 ? (
         <div className="absolute left-0">
             <img className="border-solid border-blue-fut-paz-900 border-8 
             w-2/5 rounded-full m-16 " 
-            src="henrique.jpeg" />
-        </div>
+            style={{width:'350px', height: '350px'}}
+            src={atleta?.caminhoImagem} />
+        </div>) : ""
     )
 }
